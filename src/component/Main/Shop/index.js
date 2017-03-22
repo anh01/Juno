820eb64cff71955e47754ef82176484720ce69c6
Admin <vanpho02@gmail.com>
2017-03-22 15:22:20 +0700

    scroll

--- src/component/Main/Shop/index.js
+++ src/component/Main/Shop/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
 
 export default class Shop extends Component {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text>Shop Component</Text>
+        <ScrollableTabView
+          style={{ marginTop: 2 }}
+          renderTabBar={() => <DefaultTabBar />}
+        >
+          <View tabLabel="Nổi bật">
+            <Text>A</Text>
+          </View>
+          <View tabLabel="Top">
+            <Text>B</Text>
+          </View>
+          <View tabLabel="Khuyến mại">
+            <Text>C</Text>
+          </View>
+          <View tabLabel="Bộ sưu tập">
+            <Text>D</Text>
+          </View>
+        </ScrollableTabView>
       </View>
     );
   }
